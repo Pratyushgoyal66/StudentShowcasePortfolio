9b6a179b73a75daffcba1ee7793e9343bb055d8f
Naman Goyal <namango99@gmail.com>
2021-06-04 11:05:42 +0530

    Styling changes

---
+++ README.md
@@ -0,0 +1,19 @@
+# StudentShowcasePortfolio
+
+## Btech CSE 4th year Major Project
+
+MEAN stack application.
+Required Node, angular, Mongodb and Express to run.
+Steps :-
+1. Fork the project (or download zip)
+2. Delete node modules folder.
+3. In terminal, go to project directory and run npm install.
+4. Then run nodemon
+5. Open another terminal and run mongodb (using either mongod or mongo as suitable for your system)
+6. Open another terminal, go to project directory and cd to angular-src
+7. Delete the node modules in angular-src and run npm install again.
+8. run ng serve
+9 Open the project at http://localhost:4200/ in browser
+10. You would need to create new user (register) to use the features.
+
+You can first run the project without steps 2 and 7. If encountering error, only then do steps 2 and 7 too.
---
+++ models/user.js
@@ -0,0 +1,143 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const crpyto = require('crypto');
+const config = require('../config/database');
+const Project = require('./project');
+
+//User schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type:{
+            type: String,
+            enum: ['student', 'teacher', 'university']
+        }
+    },
+    department: {
+        type: String,
+        required: function (){
+            return this.role.type === "teacher"
+        }
+    },
+    enrollmentNo: {
+        type: Number,
+        trim: true,
+        unique: function (){
+            return this.role.type === "student"
+        },
+        validate:  {
+            validator: function(value){
+                if (this.role.type !== 'student'){
+                    return true;
+                }
+                else{
+                    return /^$|^\d{11}$/.test(value);
+                }
+                
+            },
+            message: 'Invalid Enrollment Number'
+        },
+        required: function (){
+            return this.role.type === "student"
+        },
+        default: null
+
+    },
+    phoneNo: {
+        type: Number,
+        trim: true,
+        validate:  {
+            validator: function(value){
+                if (value == '' || value == null || value == undefined){
+                    return true;
+                }
+                else{
+                    return /^$|^\d{10}$/.test(value);
+                }
+                
+            },
+            message: 'Invalid Phone Number'
+        },
+        sparse: true,
+        unique: true,
+    
+    },
+    social: {
+        github:{
+            type: String,
+            trim: true,
+            sparse: true,
+            validate: [/^$|^(http(s?):\/\/)?(www\.)?github\.([a-z])+\/([A-Za-z0-9]{1,})+\/?$/i, 'Please fill a valid github profile link']
+
+        },
+        linkedIn: {
+            type: String,
+            trim: true,
+            sparse: true,
+            validate: [/^$|((https?:\/\/)?((www|\w\w)\.)?linkedin\.com\/)((([\w]{2,3})?)|([^\/]+\/(([\w|\d-&#?=])+\/?){1,}))$/i, 'Please fill a valid Linkedin Profile link']
+
+        },
+    },
+    projects: [{
+        _id: {
+            type: mongoose.Schema.Types.ObjectId, ref: 'Project'
+        },
+        title:{
+            type:String
+        }
+    }]
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
+
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    const query = {username: username};
+    User.findOne(query, callback);
+}
+
+
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
